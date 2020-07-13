ddc3df4cf5e461ad7a36bee68478afa6eefbda1c
rebekka.karrer <rebekka.karrer@tum.de>
2020-07-13 22:46:11 +0200

    comments

--- alice.py
+++
@@ -1,7 +0,0 @@
-
-import creator
-class Alice(creator.Creator):
-    def __init__(self):
-        super().__init__()
-    def one_step(self):
-        return super().create_qubit()
--- bob.py
+++
@@ -1,7 +0,0 @@
-
-import measurer
-class Bob(measurer.Measurer):
-    def __init__(self):
-        super().__init__()
-    def one_step(self, rho):
-        super().measure(rho)
